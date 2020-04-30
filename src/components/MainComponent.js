6e5361295caa2f04568491694b767c5ca8c7393a
othnielalexander <othniel0607@gmail.com>
2020-04-30 19:53:41 +0530

    Redux Thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { addComment } from "../redux/ActionCreators";
+import { addComment, fetchDishes } from "../redux/ActionCreators";
 
 const mapStatetoProps = (state) => {
   return {
@@ -22,6 +22,10 @@ const mapStatetoProps = (state) => {
 const mapDispatchtoProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) =>
     dispatch(addComment(dishId, rating, author, comment)),
+
+  fetchDishes: () => {
+    dispatch(fetchDishes());
+  },
 }); // we are using the function addComment to dispatch
 
 class Main extends Component {
@@ -29,11 +33,17 @@ class Main extends Component {
     super(props);
   }
 
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+
   render() {
     const Homepage = () => {
       return (
         <Home
-          dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+          dishesLoading={this.props.dishes.isLoading}
+          dishesErrMess={this.props.dishes.errMess}
           promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
           leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
@@ -44,10 +54,12 @@ class Main extends Component {
       return (
         <DishDetail
           dish={
-            this.props.dishes.filter(
+            this.props.dishes.dishes.filter(
               (dish) => dish.id === parseInt(match.params.dishId, 10)
             )[0]
           }
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
           comments={this.props.comments.filter(
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -8,6 +8,7 @@ import {
   BreadcrumbItem,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
 
 // function RenderMenuItem (props) {}
 function RenderMenuItem({ dish, onClick }) {
@@ -26,7 +27,7 @@ function RenderMenuItem({ dish, onClick }) {
 // function Menu (props) {}
 const Menu = (props) => {
   // map (JS keyword): iterating over every dish in the dishes array here
-  const menu = props.dishes.map((dish) => {
+  const menu = props.dishes.dishes.map((dish) => {
     return (
       // key: every item require key attribute to specift it.
       // the key helps React to recognise each one of these elements, uniquely.
@@ -36,26 +37,46 @@ const Menu = (props) => {
     );
   }); // take the JS objects
 
-  // return what needs to diaplay on the UI in this conponent. (return view)
-  return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <Breadcrumb>
-            <BreadcrumbItem>
-              <Link to="/home">Home</Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>Menu</BreadcrumbItem>
-          </Breadcrumb>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>Menu</h3>
-          <hr />
+  if (props.dishes.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
         </div>
       </div>
-      <div className="row">{menu}</div>
-    </div>
-  );
+    );
+  } else if (props.dishes.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h4>{props.dishes.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  // return what needs to diaplay on the UI in this conponent. (return view)
+  else
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <Link to="/home">Home</Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem active>Menu</BreadcrumbItem>
+            </Breadcrumb>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>Menu</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">{menu}</div>
+      </div>
+    );
 };
 
 export default Menu;
